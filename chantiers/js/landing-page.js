4690e2028af140a9bbdcca02e57c69c704cf0cde
Ismael Sane <six.echo.sa@gmail.com>
2019-12-09 18:32:25 +0100

    Add pop up modal image in chantier layout

--- chantiers/js/landing-page.js
+++ chantiers/js/landing-page.js
@@ -84,3 +84,35 @@ $('#prev').click(function() {
 $('#next').click(function() {
   moveToSelected('next');
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var pictures = [document.getElementById("myImg-1"), document.getElementById("myImg-2"),
+                document.getElementById("myImg-3"), document.getElementById("myImg-4"),
+                document.getElementById("myImg-5")];
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+// Get the image: Functional version
+function functionalModal(pictures) {
+  return pictures.map(picture => picture.onclick = function(){
+                                    modal.style.display = "block";
+                                    modalImg.src = this.src;
+                                    captionText.innerHTML = this.alt;
+                                  });
+}
+functionalModal(pictures);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
